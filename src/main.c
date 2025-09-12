f461b9895ce1002eb307f0dadeba98dbc398360f
Yassine El-Alja <yel-alja@c1r9p14.1337.ma>
2025-09-12 11:17:30 +0100

    add: some scenes

--- src/main.c
+++ src/main.c
@@ -139,15 +139,15 @@ void init_mlx(t_scene *scene)
 {
     scene->data = malloc(sizeof(t_mlx_data));
     garbage_collect(scene->data, EXIT_FAILURE);
-    memset(scene->data, 0, sizeof(t_mlx_data));
+    memset(scene->data, 0, sizeof(t_mlx_data)); //?
     scene->data->mlx = mlx_init();
     garbage_collect(scene->data->mlx, EXIT_FAILURE);
     scene->data->win = mlx_new_window(scene->data->mlx, WIDTH, HEIGHT, "miniRT");
     scene->data->img = mlx_new_image(scene->data->mlx, WIDTH, HEIGHT);
     scene->data->addr = mlx_get_data_addr(scene->data->img, &scene->data->bits_per_pixel, 
                                          &scene->data->line_length, &scene->data->endian);
-    mlx_key_hook(scene->data->win, key_hook, scene);
     mlx_hook(scene->data->win, 17, 0, __exit, scene->data);
+    mlx_key_hook(scene->data->win, key_hook, scene->data);
 }
 
 int main(int ac, char **av)
